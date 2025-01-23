def19015e7c2913c8121c6a952f0bd38237a9279
dave <Cadenadavid25@gmail.com>
2025-01-23 15:14:16 -0700

    Added TAsk 1 parts and high low calculator aswell as the while loops

--- Program.cs
+++ Program.cs
@@ -4,11 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter a low number: ");
-            int low_number = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine(low_number);
+            Console.Write("Please enter a low number: ");
+            int lowNumber = Convert.ToInt32(Console.ReadLine());
+            while (lowNumber < 0)
+            {
+                Console.Write("Positive Number Only fn");
+                Console.WriteLine("Please enter a low number: ");
+                lowNumber = Convert.ToInt32(Console.ReadLine());
+            }
 
+            Console.Write("Please enter a high number: ");
+            int highNumber = Convert.ToInt32(Console.ReadLine());
+            while (lowNumber > highNumber)
+            {
+                Console.Write("Positive Number Only fn");
+                Console.Write("Please enter a low number: ");
+                lowNumber = Convert.ToInt32(Console.ReadLine());
+            }
 
+            int differance = highNumber - lowNumber;
+            Console.WriteLine($"The differance between the numbers is {differance}");
 
         }
     }
