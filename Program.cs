8b182ce2b08634595a2de09395551afdb7bfa79a
dave <roboticviperr@gmail.com>
2025-01-23 13:18:29 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿namespace lab0_Basics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+
+
+        }
+    }
+}
